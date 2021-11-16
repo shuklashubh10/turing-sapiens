1a38c9e22a8d73b0d91b0209860a70811d6222bd
shuchitpant <emailshuchitpant@gmail.com>
2021-11-16 15:19:12 +0530

    Created new folder Final

---
+++ Final/app.js
@@ -0,0 +1,13 @@
+const menu = document.querySelector('#mobile-menu');
+const menuLinks = document.querySelector('.nav-menu');
+
+
+
+
+
+
+menu.addEventListener('click', function(){
+    menu.classList.toggle('is-active');
+    menuLinks.classList.toggle('active');
+})
+
